6939c83d4bb2512a100f7ebae80fb7add9c52c3e
Matthias Guenther <matthias@wikimatze.de>
2016-05-09 21:45:34 +0200

    No need for api

--- config.rb
+++ config.rb
@@ -3,25 +3,16 @@ require 'time'
 
 # Custom Config
 DATA_EXT = ".yml" # Should be JSON if easier
-API_PREFIX = "/api/v1"
 
 # Paths
 set :css_dir, 'public/css'
 set :js_dir, 'public/js'
 set :images_dir, 'public/images'
 
-Dir["data/*"].each do |path|
-    name = File.basename path, DATA_EXT
-    proxy "#{API_PREFIX}/#{name}.json", "api.json",
-      :locals => { :collection => name }
-end
 
 helpers do
   def markdown(data)
     Tilt['md'].new { data }.render
-  end
-  def api(page)
-    "#{API_PREFIX}/#{page}.json"
   end
 end
 
