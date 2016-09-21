b40bc40409bb002ab553ebe8833e0989d3282922
Matthias Guenther <matthias@wikimatze.de>
2016-09-21 08:31:25 +0200

    Better URLs

--- config.rb
+++ config.rb
@@ -9,11 +9,6 @@ set :css_dir, 'public/css'
 set :js_dir, 'public/js'
 set :images_dir, 'public/images'
 
-proxy '/vimfest2016', '/vimfest2016.html'
-proxy '/recap-vimfest-2015', '/recap-vimfest-2015.html'
-proxy '/recap-vimfest-2016', '/recap-vimfest-2016.html'
-
-
 helpers do
   def markdown(data)
     Tilt::KramdownTemplate.new { data }.render
